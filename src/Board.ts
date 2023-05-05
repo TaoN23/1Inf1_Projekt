4a4bec015133a95e95adde60156f826db6b15f95
taon23 <emiliannemec@gmx.de>
2023-05-05 09:13:45 +0200

    Inf-5: fehlerbehebungen

--- src/Board.ts
+++ src/Board.ts
@@ -5,7 +5,7 @@ import { baba } from "./Sprites";
 
 interface LevelString {
     meta: {},
-    board: RawBoard;
+    board: Array<string>;
 }
 
 export class Level {
@@ -23,21 +23,17 @@ export class Level {
 }
 
 
-interface RawBoard {
-    rawBoard: Array<string>
-}
-
 type ModelBoard = Array<Array<Array<Sprite>>>;
 
 class Board {
 
-    board: ModelBoard;
+    board: ModelBoard = [];
 
-    constructor(private rawBoard: RawBoard) {
-        this.board = this.parseBoard(this.rawBoard);
+    constructor(private rawBoard: Array<string>) {
+        this.parseBoard(this.rawBoard);
     }
 
-     private parseBoard(shortcut: String , leveldata: String[] ): ModelBoard {
+     private parseBoard(leveldata: String[] ): void {
         // TODO: Parse Logic 
         //this.board[y][x][length] = {x: 0, y: 0, width: 20, height: 20, type: SpriteTypes.WALL};
         
@@ -56,12 +52,13 @@ class Board {
  
                         for (z = 0; z > -1; z++) {
 
-                            this.board.getSprite(leveldata.at(column), x, row, z)
+                            // @ts-expect-error
+                            this.addSpriteData(x, row, 20, 20, shortcutMap[leveldata.at(column)]);
                          
                             
                             if (leveldata.at(column)==']') {
                                 
-                                break
+                                break;
 
                             }
 
@@ -72,17 +69,15 @@ class Board {
                     }
 
                     z = 0;
-
-                    this.board.getSprite(leveldata.at(column), x, row, z);
+                    //@ts-expect-error
+                    this.addSpriteData(x, row, 20, 20, shortcutMap[leveldata.at(column)]);
+                    x++;
 
                 }
 
 
             }
 
-
-        
-        return [[[{}]]]
     }
 
     private addSpriteData(x: number, y: number, width: number, height: number, type: SpriteTypes){
@@ -90,26 +85,30 @@ class Board {
         this.board[y][x][length] = {x, y, width_p: width, height_p: height, type};
     }
 
+
+    // View Objekte können sich eintragen
     public addSprite(sprite: Sprite, x: number, y: number) {
         const length = this.board[y][x].length;
         this.board[y][x][length].reference = sprite;
     }
 
 
-    public getSprite( shortcut: String , x: number , y: number , z: number){
-        switch (shortcut) {
+    // public getSprite( shortcut: String , x: number , y: number , z: number){
+    //     switch (shortcut) {
             
-            case '#': return null;
-            case 'b': return this.board[y][x][length] = {x: 0, y: 0, width: 20, height: 20, type: SpriteTypes.BABA}
-            case 'w': return this.board[y][x][length] = {x: 0, y: 0, width: 20, height: 20, type: SpriteTypes.WALL}
+    //         case '#': return null;
+    //         case 'b': return this.board[y][x][length] = {x: 0, y: 0, width_p: 20, height_p: 20, type: SpriteTypes.BABA}
+    //         case 'w': return this.board[y][x][length] = {x: 0, y: 0, width_p: 20, height_p: 20, type: SpriteTypes.WALL}
 
-        }
-
-
-
-    }
+    //     }
+    // }
 
 
 
 }
 
+const shortcutMap = {
+    '#': SpriteTypes.VOID,
+    'b': SpriteTypes.BABA,
+    'w': SpriteTypes.WALL,
+}
